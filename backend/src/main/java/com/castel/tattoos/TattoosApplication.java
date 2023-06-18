fa709ba4e5237f27762b559708a62fb639b3b4d4
NoCastel <castelendy@gmail.com>
2023-06-18 23:07:17 +0300

    spring boot initial

---
+++ backend/src/main/java/com/castel/tattoos/TattoosApplication.java
@@ -0,0 +1,13 @@
+package com.castel.tattoos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattoosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattoosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/castel/tattoos/TattoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castel.tattoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattoosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
